e3c6a302d3e8720f543da00d2c7f96ee4bc8b33e
krealiza <krealiza@yahoo.com>
2023-03-12 10:24:14 +0800

    testing testing

--- app/src/main/java/com/example/testapp/ui/goals/GoalsFragment.java
+++ app/src/main/java/com/example/testapp/ui/goals/GoalsFragment.java
@@ -1,4 +1,4 @@
-package com.example.testapp.ui.notifications;
+package com.example.testapp.ui.goals;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.testapp.databinding.FragmentNotificationsBinding;
+import com.example.testapp.databinding.FragmentGoalsBinding;
 
-public class NotificationsFragment extends Fragment {
+public class GoalsFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentGoalsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        GoalsViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(GoalsViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentGoalsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/testapp/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/testapp/ui/profile/ProfileViewModel.java
@@ -1,16 +1,16 @@
-package com.example.testapp.ui.notifications;
+package com.example.testapp.ui.profile;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class ProfileViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public ProfileViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is profiles fragment");
     }
 
     public LiveData<String> getText() {
